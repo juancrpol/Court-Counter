fef49d4ecbcbe7cf9f8f3ada448e44092e1ebb9d
Juan Carlos Rodriguez Polania <juancoz@hotmail.com>
2020-10-08 09:28:52 -0500

    Set buttons

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void AddThreeForTeamA(View view) {
+        displayForTeamA(3);
+    }
+
+    public void AddTwoForTeamA(View view) {
+        displayForTeamA(2);
+    }
+
+    public void AddOneForTeamA(View view) {
+        displayForTeamA(1);
     }
 }
